0d69d55396cd2d854d082855506e2754ebbf8576
TimothyKristanto <66807730+TimothyKristanto@users.noreply.github.com>
2023-04-03 13:48:20 +0700

    Initial Commit

---
+++ drakonnect_prototyping/drakonnect_prototypingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drakonnect_prototypingApp.swift
+//  drakonnect_prototyping
+//
+//  Created by Timothy on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct drakonnect_prototypingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
